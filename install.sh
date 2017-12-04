19fbfaf8b71b27f5cf38cd115b999b42d3e7127d
mansoorkodungallur <mansoorkodungallur@gmail.com>
2017-12-04 15:09:51 +0530

    instal code blocks

---
+++ install.sh
@@ -0,0 +1,24 @@
+sudo apt-get update -y
+sudo apt-get install vim terminator 
+
+
+#to install code-block latest
+
+#for latest verion
+sudo add-apt-repository ppa:damien-moore/codeblocks-stabl
+sudo apt-get update -y
+sudo apt-get install -y codeblocks
+sudo apt-get update -y
+
+cat > ~/.gdbinit << EOF
+set inferior-tty \$arg0
+EOF
+
+echo
+echo
+echo
+echo -e "\033[0;31m>>>To enable Debgger do below step \033[0m"
+echo -e "\033[0;32m   >>>In codeblocks IDE->Settings->Debugger->Default\033[0m"
+echo -e "\033[0;32m      >>>Uncheck this option: Disable Startup scripts\033[0m"
+
+
